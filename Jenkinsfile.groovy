e0790b3de2868d1ed073a9a3b3985034c32ce4bc
nsaip <nadiasaip@gmail.com>
2019-08-25 14:55:03 -0500

    terraform installation

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,9 +9,20 @@ pipeline{
                 ech Hello
                 sudo yum install httpd -y
                 ping -c 4 google.com
+                yum install telnet -y
+                yum install wget -y
                 '''
             }
         }
+        stages("Install Terraform"){
+            steps{
+                ws("tmp/"){
+                    sh "pwd"
+                    sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+
+                }
+            }
+        }
     }
-}
+
 
